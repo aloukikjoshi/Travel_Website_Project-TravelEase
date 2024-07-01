d9be45b3a48903a2aa96b35da4d5a527c2412d05
Aloukik Joshi <aloukikjoshi@gmail.com>
2024-07-01 11:19:07 +0530

    Still to be fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var map;
-var service;
-var infowindow;
+let map;
+let service;
+let infowindow;
 
 function initMap() {
     var sydney = new google.maps.LatLng(-33.867, 151.195);
@@ -10,22 +10,10 @@ function initMap() {
     map = new google.maps.Map(
         document.getElementById('map'), {center: sydney, zoom: 15});
 
-    var request = {
-        query: 'Museum of Contemporary Art Australia',
-        fields: ['name', 'geometry'],
-    };
-
     service = new google.maps.places.PlacesService(map);
 
-    service.findPlaceFromQuery(request, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-
-            map.setCenter(results[0].geometry.location);
-        }
-    });
+    // Add event listener to search button
+    document.getElementById('search-btn').addEventListener('click', searchPlaces);
 }
 
 function createMarker(place) {
@@ -59,42 +47,5 @@ function searchPlaces() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Add an event listener to the search button
-    document.getElementById('search-btn').addEventListener('click', searchPlaces);
-});
-// Add an event listener to the search button
-document.getElementById('search-btn').addEventListener('click', searchPlaces);
-
-$(document).ready(function () {
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1000, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-});
-window.onscroll = function () {
-    var navbar = document.querySelector('.navbar');
-    if (window.pageYOffset > 0) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-};
+// Initialize the map when the page loads
+window.onload = initMap;
